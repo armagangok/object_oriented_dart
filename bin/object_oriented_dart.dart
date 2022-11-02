9d5c699fb96cc11780339352c4c73fb9d50b01a0
Armağan Gök <1armagangok@gmail.com>
2022-11-02 14:52:10 +0300

    initial commit

---
+++ bin/object_oriented_dart.dart
@@ -0,0 +1,5 @@
+import 'package:object_oriented_dart/object_oriented_dart.dart' as object_oriented_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${object_oriented_dart.calculate()}!');
+}
---
+++ test/object_oriented_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:object_oriented_dart/object_oriented_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
